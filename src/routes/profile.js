0246c84d8df2498c3191089dd57ff26e4dcc6cc2
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-09-20 11:40:02 +0530

    7th commit

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,219 +1,47 @@
 import express from 'express';
 import passport from 'passport';
 
-import Profile from '../models/Profile';
-import User from '../models/User';
-
-// Load validation
-import validateProfileInput from '../validation/profileValidation';
+//Load controllers
+import profileControll from '../controllers/profileController'
 
 const router = express.Router();
-// @route   GET users/profile/all
+// @route   GET profile/all
 // @desc    Get all users' profiles
 // @access  Public
-router.get('/all', async (req, res) => {
-    const errors = {};
-    try {
-        const profiles = await Profile.find(
-            { disabled: false }
-        ).populate({
-            path: 'user',
-            select: ['firstname', 'lastname', 'email', 'isAdmin']
-        });
-
-        if (!profiles) {
-            errors.noprofile = 'This user has not created a profile';
-            return res.status(404).json();
-        }
-        res.json(profiles);
-    }
-    catch (err) {
-        res.status(404).json({ profile: 'Error retrieving profile' });
-    }
-});
+router.get('/all', profileControll.allProfiles);
 
 // @desc    Get profile/allUsers
 // @access  Admin
-router.get('/allUsers', async (req, res) => {
-    const errors = {};
-    try {
-        const users = await User.find().populate({
-            path: 'user',
-            select: ['firstname', 'lastname', 'email', 'isAdmin']
-        });
-
-        if (!users) {
-            errors.noprofile = 'This user has not created a profile';
-            return res.status(404).json();
-        }
-        res.json(users);
-    }
-    catch (err) {
-        res.status(404).json({ user: 'Error retrieving users' });
-    }
-});
+router.get('/allusers', profileControll.allUsers);
 
 // @route   GET profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
-router.get('/handle/:handle', async (req, res) => {
-    const errors = {};
-    try {
-        const profile = await Profile.findOne({ handle: req.params.handle })
-            .populate('user', ['firstname', 'lastname', 'email', 'isAdmin']);
-        if (!profile) {
-            errors.noprofile = 'There is no profile for this user';
-            res.status(404).json(errors);
-        }
-        res.json(profile);
-
-    }
-    catch (err) {
-        res.status(404).json(err);
-    }
-  });
+router.get('/handle/:handle', profileControll.handle);
 
 // @route   GET profile/
 // @desc    Get current user's profile
 // @access  Private
-router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    const errors = {};
-    try {
-        const profile =  await Profile.findOne({ user: req.user.id })
-            .populate('user', ['isAdmin']);
-        if (!profile) {
-            errors.noprofile = 'There is no profile for this user';
-            return res.status(404).json(errors);
-        }
-        res.json(profile);
-    }
-    catch (err) {
-        res.status(404).json(err);
-    }
-});
+router.get('/', passport.authenticate('jwt', { session: false }), profileControll.getProfile);
 
 
 // @route   POST profile/
 // @desc    Create or edit user profile
 // @access  Private
-router.post('/', passport.authenticate('jwt', { session: false}), (req, res) => {
-    const { errors, isValid } = validateProfileInput(req.body);
+router.post('/', passport.authenticate('jwt', { session: false}), profileControll.postProfile);
 
-    // Check validation
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    
-    // Get fields
-    const profileFields = {};
-    profileFields.user = req.user.id; 
+router.post('/disableProfile', passport.authenticate('jwt', { session: false }), profileControll.disableProfile);
 
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.major) profileFields.major = req.body.major;
-    if (req.body.minor) profileFields.minor = req.body.minor;
-    if (req.body.type) profileFields.type = req.body.type;
-    if (req.body.courses) profileFields.courses = req.body.courses;
-    profileFields.bio = req.body.bio ? req.body.bio : '';
-    profileFields.availability = req.body.availability ? req.body.availability : '';
-
-
-    Profile.findOne({ user: req.user.id }).then(profile => {
-        if (profile) {
-            // Update profile
-            Profile.findOneAndUpdate(
-                { user: req.user.id },
-                { $set: profileFields },
-                { new: true }
-            ).then(profile => res.json(profile));
-        }
-        // Profile not found
-        else {
-            Profile.findOne({ handle: profileFields.handle }).then(profile => {
-                new Profile(profileFields).save().then(profile => res.json(profile));
-            });
-            // set has profile field to true
-            User.findOne({ _id: req.user.id }).then(user => {
-                if (user) {
-                    User.findOneAndUpdate(
-                        { _id: req.user.id },
-                        { $set: { hasProfile: true }}
-                    ).then(user => res.json(user));
-                }
-            });
-        }
-    });
-});
-
-router.post('/disableProfile', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    try {
-        console.log(req.body.userId)
-        const profile = await Profile.findOneAndUpdate(
-            { user: req.body.userId},
-            { $set: { disabled: true }}
-        ).then(profile => {
-            res.json(profile);
-            User.findOneAndUpdate({ _id: req.body.userId }, {$set: {disabled: true }}).then(user => res.json(user));
-        });
-    }
-    catch (err) {
-        console.error(err);
-
-    }
-});
-
-router.post('/enableProfile', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    try {
-        console.log(req.body.userId)
-        const profile = await Profile.findOneAndUpdate(
-            { user: req.body.userId},
-            { $set: { disabled: false }}
-        ).then(profile => {
-            res.json(profile);
-            User.findOneAndUpdate({ _id: req.body.userId}, {$set: {disabled: false }}).then(user => res.json(user));
-        });
-
-    }
-    catch (err) {
-        console.error(err);
-    }
-});
+router.post('/enableProfile', passport.authenticate('jwt', { session: false }), profileControll.enableProfile);
 
 // @route   DELETE profile/
 // @desc    Delete user and profile of current logged in user
 // @access  Private
-router.delete('/', passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      Profile.findOneAndRemove({ user: req.user.id }).then(() => {
-        User.findOneAndRemove({ _id: req.user.id }).then(() =>
-          res.json({ success: true })
-        );
-      });
-    }
-);
+router.delete('/', passport.authenticate('jwt', { session: false }), profileControll.deleteProfile);
 
 // @route   DELETE profile/id
 // @desc    Delete user and profile by id (for admin)
 // @access  Private
-router.delete('/id', passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      User.findOne({ _id: req.body.id.user }).then(user => {
-          if (user) {
-              if (user.hasProfile) {
-                  Profile.findOneAndRemove({ user: req.body.id.user }).then(() => {
-                      User.findOneAndRemove({ _id: req.body.id.user }).then(() =>
-                        res.json({ success: true })
-                      );
-                  });
-              }
-              else {
-                  User.findOneAndRemove({ _id: req.body.id.user }).then(() =>
-                    res.json({ success: true })
-                  );
-              }
-
-          }
-      });
-    }
-);
+router.delete('/id', passport.authenticate('jwt', { session: false }), profileControll.deleteProfileId);
 
 module.exports = router;
